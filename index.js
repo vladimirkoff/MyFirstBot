6899f1a68248b07e08de614b5ac108a321f0d05b
Vladimir <vovakovalev2016@icloud.com>
2022-12-27 14:27:10 +0300

    Added schedule

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
 const TelegramApi = require('node-telegram-bot-api');
-const {gameOptions} = require('./options');
+const {buttonOptions} = require('./options');
 const token = '5961793778:AAHmaqiJnYGMoh5aPnsbv3MdtYIR012Bdjc';
-
 const bot = new TelegramApi(token, {polling: true});
-
-
-const chats = {};
-
 
 
 
 
 const start =  () => {
 
-
-
-
+  var week;
 
     bot.setMyCommands([
-        {command: '/start', description: 'Приветствие'},
+        {command: '/start', description: 'Запустить бота'},
         {command: '/info', description: 'Информация'},
-        {command: '/game', description: 'Game'},
-
+        {command: '/week', description: 'Выбрать неделю'},
     ])
 
     bot.on('message', async msg => {
@@ -29,30 +21,24 @@ const start =  () => {
     const chatID = msg.chat.id;  // получем айди
     if (text === '/start') {
         await bot.sendSticker(chatID, 'https://tlgrm.eu/_/stickers/b8e/030/b8e030b6-a4b6-3cac-b5a9-1d30c04d83d8/5.webp')
-        return bot.sendMessage(chatID, `Welcome!`)
+        return bot.sendMessage(chatID, `Добро пожаловать, студент КПИ!`)
     }
-    if (text === '/info') return bot.sendMessage(chatID, `Your id is  ${msg.chat.id}!`)
-    if (text === '/game') {
-        await bot.sendMessage(chatID, 'Сейчас я загадаю число от 0 до 9');
+    if (text === '/info') return bot.sendMessage(chatID, `С помощью этого бота ты сможешь узнать расписание на день, неделю, а также узнать, сколько времени осталось до конца текущего урока`)
+    if (text === '/week') {
+        await bot.sendMessage(chatID, 'Выбери неделю:', buttonOptions);
+        bot.on ('callback_query',  msg => {
+            const data = msg.data;
+            week = data;
+            return bot.sendMessage(chatID, `Неделя - ${data}`);
+        })
+        }
 
-       return bot.sendMessage(chatID, 'Отгадай', gameOptions)
 
-    }
-    else return bot.sendMessage(chatID, 'Invalid command');
+
+    else return bot.sendMessage(chatID, 'Не знаю такой комманды!');
         console.log(msg)
     })
 
-    bot.on('callback_query', async msg => {
-        const data = msg.data;
-        const ID = msg.message.chat.id;
-        const num = Math.floor(Math.random() * 10);
-
-        chats[ID] = num.toString();
-        await bot.sendMessage(ID, `Ты нажал на ${data}`);
-        if (data === chats[ID])  return bot.sendMessage(ID, 'Угадал!')
-        else  return bot.sendMessage(ID, `Нет, бот загадал цифру ${chats[ID]}`)
-        console.log(msg)
-    })
 }
 
 start();
