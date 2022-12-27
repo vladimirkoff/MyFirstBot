9ab12962f410ab3b99ae57725d2a48c9d72e7a09
Vladimir <vovakovalev2016@icloud.com>
2022-12-27 14:57:04 +0300

    Added schedule

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const TelegramApi = require('node-telegram-bot-api');
-const {buttonOptions} = require('./options');
+const {buttonOptions, daySchedule} = require('./options');
+const {firstWeek} = require('./schedule');
 const token = '5961793778:AAHmaqiJnYGMoh5aPnsbv3MdtYIR012Bdjc';
 const bot = new TelegramApi(token, {polling: true});
 
@@ -23,16 +24,10 @@ const start =  () => {
         await bot.sendSticker(chatID, 'https://tlgrm.eu/_/stickers/b8e/030/b8e030b6-a4b6-3cac-b5a9-1d30c04d83d8/5.webp')
         return bot.sendMessage(chatID, `Добро пожаловать, студент КПИ!`)
     }
-    if (text === '/info') return bot.sendMessage(chatID, `С помощью этого бота ты сможешь узнать расписание на день, неделю, а также узнать, сколько времени осталось до конца текущего урока`)
-    if (text === '/week') {
-        await bot.sendMessage(chatID, 'Выбери неделю:', buttonOptions);
-        bot.on ('callback_query',  msg => {
-            const data = msg.data;
-            week = data;
-            return bot.sendMessage(chatID, `Неделя - ${data}`);
-        })
-        }
-
+    else if (text === '/info') return bot.sendMessage(chatID, `С помощью этого бота ты сможешь узнать расписание на день, неделю, а также узнать, сколько времени осталось до конца текущего урока`)
+    else if (text === '/week') {
+        await bot.sendMessage()
+    }
 
 
     else return bot.sendMessage(chatID, 'Не знаю такой комманды!');
@@ -40,5 +35,8 @@ const start =  () => {
     })
 
 }
+
+
+//${firstWeek[day][1]}\n ${firstWeek[day][2]}\n ${firstWeek[day][3]}\n
 
 start();
--- options.js
+++ options.js
@@ -5,5 +5,14 @@ module.exports = {
                 [{text: 'Неделя 1', callback_data: '1'}, {text: 'Неделя 2', callback_data: '2'}]
             ]
         })
-    }
+    },
+    daySchedule: {
+        reply_markup: JSON.stringify({
+            inline_keyboard: [
+                [{text: 'Расписание на сегодня', callback_data: 'first'}],
+                [{text: 'Расписание на неделю', callback_data: 'second'}],
+                [{text: 'Сколько осталось до конца', callback_data: 'third'}]
+            ]
+        })
+    },
 }
